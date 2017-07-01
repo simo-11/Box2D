ffbb189be484dea032b9cbdafb221aff3bb90886
Simo Nikula <simonikula@gmail.com>
2017-07-01 14:44:40 +0300

    vs2017 update started

--- ep/vs2013.bat
+++
@@ -1,3 +0,0 @@
-rem build elastic-plastic extensions
-premake5 vs2013
-Build\vs2013\0_Box2D-EP.sln
---
+++ ep/vs2017.bat
@@ -0,0 +1,3 @@
+rem build elastic-plastic extensions
+premake5 vs2017
+Build\vs2017\0_Box2D-EP.sln
